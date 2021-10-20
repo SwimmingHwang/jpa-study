33fea7a5c85127563c5a5cb23e77bbeb517d04e5
황수영 <sy1229@humuson.com>
2021-10-21 00:04:47 +0900

    211020 persisence context

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -10,31 +10,23 @@ public class JpaMain{
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
     EntityManager em = emf.createEntityManager();
+    EntityTransaction transaction = em.getTransaction();//엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
 
-    EntityTransaction tx = em.getTransaction();
+    transaction.begin(); // [트랜잭션] 시작
 
-    tx.begin();
+    Member memberA = new Member(102L,"member2");
+    Member memberB = new Member(103L,"member3");
 
-    try{
-      EntityManager em = emf.createEntityManager();
-      EntityTransaction transaction = em.getTransaction();//엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
+    em.persist(memberA); // == 1차 캐시에 저장 & insert SQL 생성
+    em.persist(memberB); // == 1차 캐시에 저장 & insert SQL 생성
+    ///여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
 
-      transaction.begin(); // [트랜잭션] 시작
+    //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
+    transaction.commit(); // [트랜잭션] 커밋
+    // ==> flush -> db transaction commit
 
-      em.persist(memberA); // == 1차 캐시에 저장 & insert SQL 생성
-      em.persist(memberB); // == 1차 캐시에 저장 & insert SQL 생성
-      ///여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
+    transaction.commit();
 
-      //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
-      transaction.commit(); // [트랜잭션] 커밋
-      // ==> flush -> db transaction commit
-
-      tx.commit();
-    } catch (Exception e){
-      tx.rollback();
-    } finally {
-      em.close();
-    }
     emf.close();
   }
 }
