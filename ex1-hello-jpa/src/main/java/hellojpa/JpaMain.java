4e5df9bc35327ee9774eb5b2b9a57e1740d5bf4d
황수영 <sy1229@humuson.com>
2021-10-25 23:03:47 +0900

    211025 study

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,23 +12,13 @@ public class JpaMain{
     EntityManager em = emf.createEntityManager();
     EntityTransaction transaction = em.getTransaction();//엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
 
-    transaction.begin(); // [트랜잭션] 시작
+    transaction.begin();
 
-    Member memberA = new Member(102L,"member2");
-    Member memberB = new Member(103L,"member3");
-
-    em.persist(memberA); // == 1차 캐시에 저장 & insert SQL 생성
-    em.persist(memberB); // == 1차 캐시에 저장 & insert SQL 생성
-    ///여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
-
-    //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
-    transaction.commit(); // [트랜잭션] 커밋
-    // ==> flush -> db transaction commit
-
-//    영속성 컨텍스트의 변경내용을 데이터베이스에 반
+    Member memberA = new Member(102L,"member102");
+    em.persist(memberA);
+    em.flush();
 
     transaction.commit();
-
     emf.close();
   }
 }
